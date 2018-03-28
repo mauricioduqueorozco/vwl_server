ab0a7598d905487e7b756af5dd7283a2ec72cbd1
MauricioDuqueO <mduque@agmatice.com>
2018-03-27 22:57:01 -0500

    FF

--- app.js
+++ app.js
@@ -1,11 +1,53 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 const port = process.env.PORT || 8080
 
-const server = http.createServer((req, res) =>{
-  res.end("Hi server")
-})
+const server = http.createServer()
 
-server.listen((port) , _=>{
-  console.log('Server running in port ' + port);
-})
+server.on('request', onRequest)
+server.on('listening', onListening)
+
+server.listen(port)
+
+function onRequest (req, res) {
+  let uri = req.url
+
+  if (uri.startsWith('/index') || uri === '/') return serveIndex(res)
+
+  if (uri.startsWith('/app.js')) return serveApp(res)
+
+  res.statusCode = 404
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(`404 Not found: ${uri}`)
+}
+
+function serveIndex (res) {
+  let index = path.join(__dirname, 'public', 'index.html')
+  let rs = fs.createReadStream(index)
+
+  res.setHeader('Content-Type', 'text/html')
+  rs.pipe(res)
+
+  rs.on('error', function (err) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(err.message)
+  })
+}
+
+function serveApp (res) {
+  let app = path.join(__dirname, 'public', 'app.js')
+  let rs = fs.createReadStream(app)
+
+  res.setHeader('Content-Type', 'text/javascript')
+  rs.pipe(res)
+
+  rs.on('error', function (err) {
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(err.message)
+  })
+}
+
+function onListening () {
+  console.log(`Server running in port ${port}`)
+}
